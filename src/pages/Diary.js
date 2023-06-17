2eaf12a6ee29ff4edd7871eeec24ca337fa104a9
hyeonsu <105619531+SSSSSSu3834@users.noreply.github.com>
2023-06-17 16:17:51 +0900

    페이지 라우팅

---
+++ src/pages/Diary.js
@@ -0,0 +1,13 @@
+import { useParams } from "react-router-dom";
+
+const Diary = () => {
+  const { id } = useParams();
+  return (
+    <div>
+      <h1>Diary</h1>
+      <p>일기 상세 페이지</p>
+    </div>
+  );
+};
+
+export default Diary;
